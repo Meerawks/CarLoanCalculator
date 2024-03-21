ae4780967c5969465e396bdeb56e28a4945675ee
meerawks <sameer.asif.mughal@gmail.com>
2024-03-22 03:52:49 +0500

    Added commas for numeric inputs

--- script.js
+++ script.js
@@ -19,7 +19,12 @@ vendorSelector.addEventListener('change',()=>{
     changeCar(vendorSelector.value);
 })
 
-
+price.addEventListener("keyup",(event)=>{
+  console.log("key uip");
+  var tempNumber = price.value.replace(/,/gi, "");
+  var commaSeparatedNumber = tempNumber.split(/(?=(?:\d{3})+$)/).join(",");
+  price.value = commaSeparatedNumber;
+ })
    
 
 function calculate(){
@@ -54,7 +59,7 @@ carType.addEventListener('change',()=>{
         price.placeholder='Car price'
         price.maxLength=12;
         price.min='0';
-        price.type='number';
+        price.type='text';     
         parent.replaceChild(price,tenure);
         parent.replaceChild(tenure,carDelivery);
 
@@ -62,9 +67,10 @@ carType.addEventListener('change',()=>{
     else if(carType.value=='New car'){
     
         removeOptions(carSelector);
+    
         var opt = document.createElement('option');
         opt.value = '';
-        opt.innerHTML = 'Select car';
+        opt.innerHTML = 'Select car';S
         carSelector.appendChild(opt);
         changeCar(vendorSelector.value);
         let parent=price.parentNode;
@@ -87,7 +93,7 @@ function calculateLoan(carType,vendor,car,carPrice,tenure,deposit,delivery){
 
     const price=document.createElement('p');
     price.classList.add('sub');
-    price.innerHTML='<b class=price>Price of vehicle :</b> '+carPrice;
+    price.innerHTML='<b class=price>Price of vehicle :</b> '+carPrice.toLocaleString();
 
     const upfront=document.createElement('h2');
     upfront.classList.add('head');
@@ -95,7 +101,7 @@ function calculateLoan(carType,vendor,car,carPrice,tenure,deposit,delivery){
 
     const security=document.createElement('p');
     security.classList.add('sub');
-    security.innerHTML='Security Deposit :   '+Math.round(parseInt(carPrice)*parseInt(deposit)/100)+" ("+deposit+')';
+    security.innerHTML='Security Deposit :   '+Math.round(parseInt(carPrice.replace(/,/g, ''))*parseInt(deposit)/100).toLocaleString()+" ("+deposit+')';
 
     const fee=document.createElement('p');
     fee.classList.add('sub');
@@ -103,7 +109,7 @@ function calculateLoan(carType,vendor,car,carPrice,tenure,deposit,delivery){
 
     const total=document.createElement('p');
     total.classList.add('sub');
-    total.innerHTML='Total Upfront :     '+Math.round(2500+parseInt(carPrice)*parseInt(deposit)/100);
+    total.innerHTML='Total Upfront :     '+Math.round(2500+parseInt(carPrice.replace(/,/g, ''))*parseInt(deposit)/100).toLocaleString();
 
 
     const monthly=document.createElement('h2');
@@ -116,7 +122,8 @@ function calculateLoan(carType,vendor,car,carPrice,tenure,deposit,delivery){
 
     const rent=document.createElement('p');
     rent.classList.add('sub');
-    rent.innerHTML='Rent per month :       '+Math.round(Math.round(carPrice-Math.round(2500+parseInt(carPrice)*parseInt(deposit)/100))/(parseInt(tenure)*12));
+    console.log(parseInt(carPrice.replace(/,/g, '')));
+    rent.innerHTML='Rent per month :       '+Math.round(Math.round(carPrice.replace(/,/g, '')-Math.round(2500+parseInt(carPrice.replace(/,/g, ''))*parseInt(deposit)/100))/(parseInt(tenure)*12)).toLocaleString();
 
     const button=document.createElement('button');
     button.classList.add('button');
